6aa6bfaa7d8af70c30ecbda0de573924a6f5d910
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-04-26 13:30:49 +0700

    update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require('express');
 var request = require('request');
 var router = express();
 
+const token = 'EAAEtSZC8DOskBAG4EaEaPJb2LD299exZCthNIP0l6In1rsvySksNzMRBPX9wXUf2yYjcv6GZCPZCAdwxNEzYNwhOZAnkysq8YZAYRXKO3JqfKyPQT1fMkkvXpjx62yTQG6FGaEl29fMjKG8ZCd9bFyoyDbTfJqWD3ni2sYppZAB3NwZDZD';
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,12 +23,52 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'day_la_cai_token') {
+  if (req.query['hub.verify_token'] === 'randomToken') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
 
+
+//user send message to bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+//send info to RestAPI to answer
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "token",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
