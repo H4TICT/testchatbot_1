8c53f6e0807633a717d9623d50b8674571528b77
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-03 11:05:53 +0700

    server ver.1

--- server.js
+++ server.js
@@ -1,157 +1,73 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-const Cosmic = require('cosmicjs')
-const BootBot = require('bootbot')
-require('dotenv').config()
-const chrono = require('chrono-node')
-var schedule = require('node-schedule')
-const EventEmitter = require('events').EventEmitter
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var router = express();
 
-const verifyToken = 'DogLover49'
-const accessToken = 'EAAeLpZCmj8J8BABLp8CdUhYJpKYZBj4aGnQIWkRn6ZADvYj5Ks9hZATMwUASAgaurOIv3QyzPdKhY4y710DIAJOCOWnDYrQI871YeP0zyAmsThLVvIyMu4h3WDwriCZATHQjKDrLLBvtQAuK5Rays87CcT9isxw9JgtY7CAQgs62MwzEsTX77LS2XssEeW2MZD'
-const appSecret = 'b0ca7f784b70a133090fbafd6eb4e0e3'
-
-var config = {}
-
-const reminders = []
-
-const eventEmitter = new EventEmitter()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-app.get('/', function(req, res) {
-  res.send("Server is running !!!")
-})
-
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === verifyToken){
-    return res.send(req.query['hub.challenge'])
-  }
-  res.send('wrong token')
-})
-
-app.listen(app.get('port'), function(){
-  console.log('Started on port', app.get('port'))
-})
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
 
 
-const bot = new BootBot({
-  accessToken,
-  verifyToken,
-  appSecret
-})
-
-bot.setGreetingText("Hello, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ")
-
-bot.setGetStartedButton((payload, chat) => {
-  if(config.bucket === undefined){
-    chat.say('Hello my name is Note Buddy and I can help you keep track of your thoughts')
-    chat.say("It seems like you have not setup your bucket settings yet. That has to be done before you can do anything else. Make sure to type 'setup'")
-  }
-  BotUserId = payload.sender.id
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
 });
 
-bot.hear('setup', (payload, chat) => {
-  const getBucketSlug = (convo) => {
-    convo.ask("What's your buckets slug?", (payload, convo) => {
-      var slug = payload.message.text;
-      convo.set('slug', slug)
-      convo.say("setting slug as "+slug).then(() => getBucketReadKey(convo));
-    })
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'randomToken') {
+    res.send(req.query['hub.challenge']);
   }
-  const getBucketReadKey = (convo) => {
-    convo.ask("What's your buckets read key?", (payload, convo) => {
-      var readkey = payload.message.text;
-      convo.set('read_key', readkey)
-      convo.say('setting read_key as '+readkey).then(() => getBucketWriteKey(convo))
-    })
-  }
-  const getBucketWriteKey = (convo) => {
-    convo.ask("What's your buckets write key?", (payload, convo) => {
-      var writekey = payload.message.text
-      convo.set('write_key', writekey)
-      convo.say('setting write_key as '+writekey).then(() => finishing(convo))
-    })
-  }
-  const finishing = (convo) => {
-    var newConfigInfo = {
-      slug: convo.get('slug'),
-      read_key: convo.get('read_key'),
-      write_key: convo.get('write_key')
-    }
-    config.bucket = newConfigInfo
-    convo.say('All set :)')
-    convo.end();
-  }
-  
-  chat.conversation((convo) => {
-    getBucketSlug(convo)
-  })
-})
+  res.send('Error, wrong validation token');
+});
 
-bot.hear(['hello', 'hey', 'sup'], (payload, chat)=>{
-  chat.getUserProfile().then((user) => {
-    chat.say(`Hey ${user.first_name}, How are you today?`)
-  })
-})
 
-bot.hear('config', (payloadc, hat) => {
-  if(JSON.stringify(config.bucket) === undefined){
-    chat.say("No config found :/ Be sure to run 'setup' to add your bucket details")
-  }
-  chat.say("A config has been found :) "+ JSON.stringify(config.bucket))
-})
-
-bot.hear('create', (payload, chat) => {
-  chat.conversation((convo) => {
-    convo.ask("What would you like your reminder to be? etc 'I have an appointment tomorrow from 10 to 11 AM' the information will be added automatically", (payload, convo) => {
-      datetime = chrono.parseDate(payload.message.text)
-      var params = {
-        write_key: config.bucket.write_key,
-        type_slug: 'reminders',
-        title: payload.message.text,
-        metafields: [
-         {
-           key: 'date',
-           type: 'text',
-           value: datetime
-         }
-        ]
-      }
-      Cosmic.addObject(config, params, function(error, response){
-        if(!error){
-          eventEmitter.emit('new', response.object.slug, datetime)
-          convo.say("reminder added correctly :)")
-          convo.end()
-        } else {
-          convo.say("there seems to be a problem. . .")
-          convo.end()
+//user send message to bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); //text: message from user
+          sendMessage(senderId, "Hello, I'm bot. You typed: " + text);
         }
-      })
-    })
-  })
-})
-
-bot.hear('active', (payload, chat) => {
-  chat.say('finding all of your ongoing reminders.')
-})
-
-eventEmitter.on('new', function(itemSlug, time) {
-  schedule.scheduleJob(time, function(){
-    Cosmic.getObject(config, {slug: itemSlug}, function(error, response){
-      if(response.object.metadata.date == new Date(time).toISOString()){
-        bot.say(BotUserId, response.object.title)
-        console.log('firing reminder')
-      } else {
-        eventEmitter.emit('new', response.object.slug, response.object.metafield.date.value)
-        console.log('times do not match checking again at '+response.object.metadata.date)
       }
-    })
-  })
-})
+    }
+  }
 
-bot.start()
+  res.status(200).send("OK");
+});
+
+//send info to RestAPI to answer
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "EAAeLpZCmj8J8BAN8sFu7DEvemfE7cHETzOxVFlqqwZAmFoAHf1d4U396t7MI0LoKISFGOSjQYXMoq3rvSIzifobxy8Aq8ZAuTBK49aKY6sSJBUWo5EDFjUAMncvurF7FsKoKehM6JMfnOvMkmCxTbD2OM5ZAS8zjUdfFgKHJ8IUNalee7ec8feDZBd5u6jwEZD",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+app.set('port', process.env.PORT || 5000);
+app.set('ip', process.env.IP || "0.0.0.0");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
