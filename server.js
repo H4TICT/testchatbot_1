57e0cfe9a446a201d42581f6c064a1eb7355fb28
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-03 16:53:12 +0700

    update server ver.3

--- server.js
+++ server.js
@@ -174,8 +174,8 @@ const callSendAPI = (sender_psid, response, cb = null) => {
 //     }
 //   });
 // }
-app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "0.0.0.0");
+app.set('port', PORT || 5000);
+app.set('ip', IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
