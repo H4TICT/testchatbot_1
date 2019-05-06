620ba7aef9b90a94d39a58ec7d83501c162d3b0b
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-06 16:32:20 +0700

    update server v.3 + text

--- server.js
+++ server.js
@@ -44,10 +44,10 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
     body.entry.forEach(function(entry) {
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      // console.log(webhook_event);
 
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
+      // console.log('Sender PSID: ' + sender_psid);
 
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
