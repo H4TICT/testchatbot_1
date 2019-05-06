63ef12f856cad18f67043b5f834187e03bac382e
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-06 14:47:28 +0700

    update server v.3 + text

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ const handlePostback = (sender_psid, received_postback) => {
     response = askTemplate('Choose a topic below then we can find you a friend');
     callSendAPI(sender_psid, response);
   } else{
-    sendMessage(sender_psid, sender_psid + " sent you a message");
+    sendMessage(content);
   }
 };
 
