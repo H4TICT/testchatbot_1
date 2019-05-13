0f7110d2b6b54a8c85f02290268395432d834548
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-13 17:46:01 +0700

    update database demo 1 13-May

--- model/db.service.js
+++ model/db.service.js
@@ -35,7 +35,7 @@ app.post('/user', function(req, res) {
     });
   });
 
-  app.set('port', process.env.PORT || 5000);
+  app.set('port', process.env.PORT || 3000);
   app.set('ip', process.env.IP || "0.0.0.0");
 
   server.listen(app.get('port'), app.get('ip'), function() {
