f93c6318bf3032913e9f948ea4bad9a51b203b88
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-21 15:23:40 +0700

    update db on Mongo Atlas 21-May

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ app.use(bodyParser.urlencoded({
 
 
 //database url
-// var db = 'mongodb+srv://tuanha1709:Hatuan1997hd@freechatdbtest-uy890.mongodb.net/test?retryWrites=true';
-var db = 'mongodb://localhost:27017/freechat';
+var db = 'mongodb+srv://tuanha1709:Hatuan1997hd@freechatdbtest-uy890.mongodb.net/test?retryWrites=true';
+// var db = 'mongodb://localhost:27017/freechat';
+
 mongoose.Promise = global.Promise;
 mongoose.connect(db);
 
