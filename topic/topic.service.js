723cc28084d59c2ac44d8e2e9f2c75d546bf17e5
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-05-22 17:09:08 +0700

    try POST TOPIC 22-May

--- topic/topic.service.js
+++ topic/topic.service.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const app = express();
-const router = express.Router();
+const app = express();
+// const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
