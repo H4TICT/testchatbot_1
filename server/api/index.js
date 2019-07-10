e9676ec65f4730dcf5174b3998cbad0bf3e943ae
Ha Anh Tuan <hatuan1997hd@gmail.com>
2019-07-10 17:09:11 +0700

    update

--- server/api/index.js
+++ server/api/index.js
@@ -19,6 +19,9 @@ exports.register = function (app) {
     router.use('/user', require('./user/index'));
     router.use('/topic', require('./topic/index'));
     router.use('/conv', require('./conv/index'));
+    router.get('/', (req, res) => {
+        res.send("Home page. Server running okay.");
+      })
     app.use("/api", router);
 };
 // module.exports = router ;
